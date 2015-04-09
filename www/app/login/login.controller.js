074458c9af0da9e57bef0f9037ba295540c874c9
MattDionis <mattdionis@gmail.com>
2015-04-09 18:45:24 -0400

    Add email/password and Facebook connect to sign up form

--- www/app/login/login.controller.js
+++ www/app/login/login.controller.js
@@ -7,34 +7,38 @@ LoginController.$inject = ['authService'];
 function LoginController(authService) {
 
   var vm = this;
-  vm.note = 'testing';
 
   vm.createUser = function() {
-
-    vm.message = null;
-    vm.error = null;
 
     authService.$createUser({
       email: vm.email,
       password: vm.password
     }).then(function(userData) {
-      vm.message = 'User created with uid: ' + userData.uid;
+      alert('User created with uid: ' + userData.uid);
     }).catch(function(error) {
-      vm.error = error;
+      alert(error);
     });
   };
 
   vm.removeUser = function() {
-    vm.message = null;
-    vm.error = null;
 
     authService.$removeUser({
       email: vm.email,
       password: vm.password
     }).then(function() {
-      vm.message = 'User removed';
+      alert('User removed');
     }).catch(function(error) {
-      vm.error = error;
+      alert(error);
+    });
+  };
+
+  // login with Facebook
+  vm.connectFacebook = function() {
+
+    authService.$authWithOAuthPopup("facebook").then(function(authData) {
+      alert("Logged in as:", authData.uid);
+    }).catch(function(error) {
+      alert("Authentication failed:", error);
     });
   };
 }
